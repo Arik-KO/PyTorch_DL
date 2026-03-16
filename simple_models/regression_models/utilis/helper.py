6276e2ffa9f4cf20ca36ccb03ec7b6d0111097f6
Arik-KO <arikanik40@gmail.com>
2026-03-16 13:29:28 +0800

    new utility function added for visualization

--- simple_models/regression_models/utilis/helper.py
+++ simple_models/regression_models/utilis/helper.py
@@ -15,10 +15,30 @@ def plot_losses(train_loss:list, val_loss:list, fig_name:str):
     plt.show()
 
 
-def visualize_performance(y_train, y_hat, fig_name):
-    pass
+def visualize_performance(y_train:list, y_hat:list, fig_name:str)-> None:
+    plt.figure(figsize = (10,6), dpi =150)
+    plt.scatter(y_train, y_hat, color = 'steelblue',alpha = 0.6, s=8)
+    lims = [min(y_train.min(), y_hat.min()),
+            max(y_train.max(),y_hat.max())]
+    plt.plot(lims,lims, 'r--', linewidth =2, label = "Prediction" )
+    plt.xlabel("true value")
+    plt.ylabel("predicted value")
+    plt.grid(True, alpha = 0.5)
+    plt.tight_layout()
+    plt.savefig(f"results/plots/evaluation_{fig_name}.jpeg", dpi =150, bbox_inches = 'tight')
+    plt.show()
 
+    residuals = y_hat - y_train
+    plt.figure(figsize=(10,6), dpi = 150)
+    plt.hist(residuals,bins =50, color = 'coral', alpha = 0.6)
+    plt.xlabel('prediction_error')
+    plt.ylabel('Frequency')
+    plt.grid(True, alpha = 0.5)
+    plt.tight_layout()
+    plt.save_fig(f"results/plots/histogram_{fig_name}.jpeg")
+    plt.show()
 
+    
 def save_model(model, path:str):
     torch.save(model.state_dict(), path)
     print(f"Model saved to {path}")
