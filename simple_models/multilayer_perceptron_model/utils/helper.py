f880ef0d7b9912609d67e8c88c7b040364d3beb1
Arik-KO <arikanik40@gmail.com>
2026-04-03 12:36:51 +0800

    helper functions added

---
+++ simple_models/multilayer_perceptron_model/utils/helper.py
@@ -0,0 +1,51 @@
+import torch
+import matplotlib.pyplot as plt
+
+
+def plot_loss(train_loss, val_loss, fig_name, lw = 2):
+    plt.figure(figsize=(10,6), dpi = 200)
+    plt.plot(train_loss, linewidth = lw, label = 'Training Loss', color = 'royalblue')
+    plt.plot(val_loss, linewidth = lw, linestyle = '--', color = 'coral', label = 'Validation Loss')
+    plt.grid(True, alpha= 0.8)
+    plt.legend()
+    plt.xlabel('Epochs')
+    plt.ylabel('Calculated Loss')
+    plt.tight_layout()
+    plt.savefig(f"results/plots/loss_graph_{fig_name}.png", dpi = 200, bbox_inches = 'tight')
+    plt.show()
+
+
+def visualize_performance(y_train, y_hat, fig_name, lw = 2):
+    plt.figure(figsize = (10,6), dpi =150)
+    plt.scatter(y_train, y_hat, color = 'steelblue',alpha = 0.6, s=8)
+    lims = [min(y_train.min(), y_hat.min()),
+            max(y_train.max(),y_hat.max())]
+    plt.plot(lims,lims, 'r--', linewidth =lw, label = "Prediction" )
+    plt.xlabel("true value")
+    plt.ylabel("predicted value")
+    plt.grid(True, alpha = 0.5)
+    plt.tight_layout()
+    plt.savefig(f"results/plots/evaluation_{fig_name}.jpeg", dpi =150, bbox_inches = 'tight')
+    plt.show()
+
+    residuals = y_hat - y_train
+    plt.figure(figsize=(10,6), dpi = 150)
+    plt.hist(residuals,bins =50, color = 'coral', alpha = 0.6)
+    plt.axvline(0, color='black', linewidth=lw, label='Zero residual')
+    plt.axvline(residuals.mean(), color='red', linestyle='--',
+                label=f'Mean residual = {residuals.mean():.3f}')
+    plt.xlabel('prediction_error')
+    plt.ylabel('Frequency')
+    plt.legend()
+    plt.grid(True, alpha = 0.5)
+    plt.tight_layout()
+    plt.savefig(f"results/plots/histogram_{fig_name}.jpeg")
+    plt.show()
+
+def save_model(model, path:str) -> None:
+    torch.save(model.state_dict(), path)
+    print(f"model saved to {path}")
+
+def load_model(model, path:str) :
+    model.load_state_dict(torch.load(path))
+    return model
