9e62bc6e8530151d67b0054be17e3168eea859c5
Arik-KO <arikanik40@gmail.com>
2026-04-09 19:21:27 +0800

    initial configuration of the project added

--- housing_price_project/config.py
+++ housing_price_project/config.py
@@ -1,1 +1,14 @@
+import torch
+
+
 RANDOM_STATE = 42
+LAMBDA = 0.0
+DROPOUT = 0.0
+BATCH_SIZE = 64
+LEARNING_RATE = 1e-4
+EPOCHS = 100
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+MODEL_NAME = 'Price_Prediction_Neural_Network_v1'
+
+if __name__ == "__main__":
+    print(DEVICE)
